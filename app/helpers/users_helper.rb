0e1c1140f65dcf0d05f15ec17efddafc3c20883f
derek <derekle.creative@gmail.com>
2021-06-30 09:44:08 -0500

    add is unique check

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,6 +2,10 @@ module UsersHelper
     def logged_in?
         !!session[:user_id]
     end
+    
+    def is_unique?
+        User.find_by_username(params[:username]) == nil
+    end
 
     def current_user
         User.find(session[:user_id])
