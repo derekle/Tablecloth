928346f7081556b16bc5d793948e739e8310ea9d
derek <derekle.creative@gmail.com>
2021-07-02 02:24:15 -0500

    add various methods for user checks

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+    def authorized(user)
+        current_user.id == user.id
+    end
+
+    def current_user
+        @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    end
+
+    def require_login
+        if current_user
+        else
+          redirect_to '/signin'
+        end
+    end
 end
