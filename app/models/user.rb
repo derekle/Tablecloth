b4c0dfc7c1476bf019c420560b93e47743a6305d
derek <derekle.creative@gmail.com>
2021-07-02 02:18:31 -0500

    add validations for usermodel

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,11 @@ class User < ApplicationRecord
     has_many :tables
     has_many :orders, through: :tables
     has_secure_password
-    validates :username, presence: true, uniqueness: true
-    validates :password_digest, presence: true
-    validates :email, presence: true, uniqueness: true
+    validates_confirmation_of :username, uniqueness: true, format: { without: /[0-9]/, message: "does not allow numbers" }
+    validates_confirmation_of :password, presence: true
+    validates :email, presence: true
+
+    def is_unique?
+        User.find_by_username(params[:username]) == nil
+    end
 end
