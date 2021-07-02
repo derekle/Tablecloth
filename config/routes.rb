4113db32f82cb94dc728fb95b741e7dc3ecb8325
derek <derekle.creative@gmail.com>
2021-07-02 02:20:26 -0500

    add routes for signup, login, logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,19 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   resources :menuitems
   resources :orders
   resources :tables
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root to: "users#index"
+  
+  resources :sessions, only: [:new, :create, :destroy]
+
+  get '/signin', to: 'sessions#new'
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+  get '/login', to: 'sessions#new' 
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 end
