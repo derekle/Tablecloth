81c9a00bfd2d29b53e91e96a409258f0a04e6704
derek <derekle.creative@gmail.com>
2021-07-02 02:22:26 -0500

    logic for creating a user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,49 @@
 class UsersController < ApplicationController
+    before_action :require_login
+    skip_before_action :require_login, only: [:index, :new, :create]
+
+    def new
+        @user = User.new
+    end
+
+    def index
+        if session[:id] == nil
+        render :index
+        end
+    end
+
+    def create
+        @user = User.new(user_params)
+        binding.pry
+        if @user.valid?
+            @user.save
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
+        else
+            render :new
+        end
+    end 
+
+    def edit
+    end
+
+    def show
+        @user = User.find(params[:id])
+    end
+
+    def destroy
+    end
+
+    private
+    def require_login
+        return head(:forbidden) unless session.include? :user_id
+    end
+    
+    def user_params
+      params.require(:user).permit(:username, :email, :password, :password_confirmation, :employee_type)
+    end
 end
+
+
+
+
