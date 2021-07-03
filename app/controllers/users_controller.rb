fc0e921415970d3dc68a50d01830601b5517d1a3
derek <derekle.creative@gmail.com>
2021-07-03 14:10:27 -0500

    logic for updating user models

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -31,6 +31,22 @@ class UsersController < ApplicationController
         @user = User.find(params[:id])
     end
 
+    def update
+        if !user_params[:username].empty?
+			current_user.update(username:user_params[:username])
+        end
+        if !user_params[:password].empty?
+            current_user.update(password:user_params[:password])
+        end
+        if !user_params[:email].empty?
+            current_user.update(email:user_params[:email])
+        end
+        if !user_params[:employee_type].empty?
+            current_user.update(employee_type:user_params[:employee_type])
+        end
+        render :index
+    end
+
     def destroy
     end
 
@@ -40,8 +56,7 @@ class UsersController < ApplicationController
     end
     
     def user_params
-      params.require(:user).permit(:username, :email, :password, :password_confirmation, :employee_type)
-    end
+      params.require(:user).permit(:username, :email, :password, :password_confirmation, :employee_type).to_h    end
 end
 
 
