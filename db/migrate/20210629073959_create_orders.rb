7d452534af0519779cb01baf06271fc44082b5c6
derek <derekle.creative@gmail.com>
2021-07-03 14:21:10 -0500

    add ids to take adv of AR relations

--- db/migrate/20210629073959_create_orders.rb
+++ db/migrate/20210629073959_create_orders.rb
@@ -3,7 +3,8 @@ class CreateOrders < ActiveRecord::Migration[6.1]
     create_table :orders do |t|
       t.float :total
       t.boolean :ispaid
-
+      t.integer :table_id
+      t.integer :user_id
       t.timestamps
     end
   end
