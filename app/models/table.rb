a975c7073a0eb722cd79b7f220f6c1063d4c56a4
derek <derekle.creative@gmail.com>
2021-07-03 14:12:32 -0500

    fix table relationship

--- app/models/table.rb
+++ app/models/table.rb
@@ -1,6 +1,5 @@
 class Table < ApplicationRecord
     has_many :orders
-    has_many :menuitems, through: :orders
-    belongs_to :user
-    validates :name, presence: true
+    has_many :users, through: :orders
+
 end
