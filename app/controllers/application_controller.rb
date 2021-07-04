62e936548f090755f71137c945e992313c63b924
derek <derekle.creative@gmail.com>
2021-07-04 18:32:38 -0500

    add flash types, new validation methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,25 @@
 class ApplicationController < ActionController::Base
-    def authorized(user)
-        current_user.id == user.id
-    end
+    add_flash_types :info, :error, :warning
 
     def current_user
         @current_user ||= User.find(session[:user_id]) if session[:user_id]
     end
 
+    def logged_in?
+        !!session[:user_id]
+    end
+
     def require_login
-        if current_user
-        else
-          redirect_to '/signin'
+        unless logged_in?
+          flash[:error] = "You must be logged in to access this section"
+          redirect_to root_path
         end
     end
+    def require_admin
+        unless current_user.employee_type == "manager"
+          flash[:error] = "You must be a manager to view this section"
+          redirect_to home_path
+        end
+    end
+    
 end
