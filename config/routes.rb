8cc0521f2f68a8ae4a4266adb7a5120dfb1ee41c
derek <derekle.creative@gmail.com>
2021-07-04 06:11:38 -0500

    add as: options for url helpers

--- config/routes.rb
+++ config/routes.rb
@@ -9,11 +9,13 @@ Rails.application.routes.draw do
   root to: "users#index"
   
   resources :sessions, only: [:new, :create, :destroy]
+  resources :tables
 
-  get '/signin', to: 'sessions#new'
-  get '/signup', to: 'users#new'
+  get '/signup', to: 'users#new', as: 'signup'
+  get '/employees', to: 'users#employees', as: 'employees'
+
   post '/signup', to: 'users#create'
-  get '/login', to: 'sessions#new' 
+  get '/login', to: 'sessions#new', as: 'signin'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
   get 'auth/google_oauth2/callback', to: 'sessions#create'
