4c5c023b30760b574d10008d6f30d6e5c0a5bc82
derek <derekle.creative@gmail.com>
2021-07-04 06:07:09 -0500

    add scope methods, email validation, oauth e_type

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,15 +5,17 @@ class User < ApplicationRecord
     has_secure_password
     validates :username, presence: true, uniqueness: true, format: { without: /[0-9]/, message: "does not allow numbers" }
     validates_confirmation_of :password, presence: true
-    validates :email, presence: true
+    validates :email, uniqueness: true, presence: true
     validates :employee_type, presence: true
 
     scope :get_managers, -> { where("employee_type = 'manager'") }
-
-    def is_unique?
-        User.find_by_username(params[:username]) == nil
-    end
-    
+    scope :get_cooks, -> { where("employee_type = 'cooks'") }
+    scope :get_waitstaff, -> { where("employee_type = 'waitstaff'") }
+    scope :get_bussers, -> { where("employee_type = 'bussers'") }
+    scope :get_bartenders, -> { where("employee_type = 'bartenders'") }
+    scope :get_hosts, -> { where("employee_type = 'hosts'") }
+    scope :get_needsrole, -> { where("employee_type = 'needs role'") }
+   
     def self.from_omniauth(auth)
       where(email: auth.info.email).first_or_initialize do |user|
         user.username = auth.info.name
@@ -21,6 +23,8 @@ class User < ApplicationRecord
         user.password = SecureRandom.hex
         if User.where("employee_type = 'manager'").empty?
           user.employee_type = "manager"
+        else
+          user.employee_type = "needs role"
         end
       end
   end
