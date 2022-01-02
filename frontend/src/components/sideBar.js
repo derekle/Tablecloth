1f09aaa170ec00bc24b6507278eaf8ee1bfb5b4b
DEREK LE <derekle.develop@gmail.com>
2022-01-02 11:55:00 -0700

    code: create an arbitrary sidebar

---
+++ frontend/src/components/sideBar.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Sidebar = () => {
+    return (
+        <div>
+            <ul>
+                <li>Account</li>
+                <li>Tables</li>
+                <li>Menu</li>
+                <li>Orders</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Sidebar
