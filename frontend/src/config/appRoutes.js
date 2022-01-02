1fa17f17c854d5d30fc776fd38364828dcfb3f46
DEREK LE <derekle.develop@gmail.com>
2022-01-02 11:55:00 -0700

    code: create a route to dashbaord

--- frontend/src/config/appRoutes.js
+++ frontend/src/config/appRoutes.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Dashboard from '../views/Dashboard';
 
 import Home from '../views/Home';
-
 const AppRoutes = (props) => {
 	return (
 		<div>
@@ -13,7 +13,10 @@ const AppRoutes = (props) => {
 						path="/"
 						element={<Home />}
 					/>
-
+					<Route
+						exact path='/dashboard'
+						element={<Dashboard />}
+					/>
 					<Route
 						path="*"
 						element={
