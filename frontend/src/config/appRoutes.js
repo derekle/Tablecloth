52281bc944a0627b5528cbc3520236a6b68aa740
DEREK LE <derekle.develop@gmail.com>
2022-01-16 23:17:35 -0700

    code: implement hooks for approutes

--- frontend/src/config/appRoutes.js
+++ frontend/src/config/appRoutes.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Dashboard from '../views/Dashboard';
 import Products from '../views/Products'
 import Home from '../views/Home';
+
+import { useDispatch, useSelector } from 'react-redux'
+import { fetchProduct } from '../actions/fetchProduct';
+
 const AppRoutes = (props) => {
+	const dispatch = useDispatch()
+
+	useEffect(() => {
+		dispatch(fetchProduct(null, 'GET'))
+	}, []);
+
+	const products = useSelector((state) => state.products.products)
+	console.log(products)
 	return (
 		<div>
+
 			<BrowserRouter>
 				<Routes>
 					<Route
@@ -19,7 +32,7 @@ const AppRoutes = (props) => {
 					/>
 					<Route
 						exact path='/products'
-						element={<Products />}
+						element={<Products store_id='1' products={products} />}
 					/>
 					<Route
 						path="*"
