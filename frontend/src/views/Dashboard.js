eb07f8a3f3a181ca7ec5e2ea803d286e2b86e2e6
DEREK LE <derekle.develop@gmail.com>
2022-01-16 23:18:46 -0700

    fix: fetch when dashboard loads

--- frontend/src/views/Dashboard.js
+++ frontend/src/views/Dashboard.js
@@ -1,19 +1,11 @@
 import '../css/dashboard.css'
 import React from 'react';
-import { useState, useEffect } from 'react';
-import { useDispatch } from 'react-redux'
+import { useState } from 'react';
 
 import Sidebar from '../components/sidebar/sideBar';
 import List from '../components/sidebar/list';
-import { fetchProduct } from '../actions/fetchProduct';
 const Dashboard = () => {
 	const [visible, setVisible] = useState(true)
-	const dispatch = useDispatch()
-
-	useEffect(() => {
-		dispatch(fetchProduct(null, 'GET'))
-	}, []);
-
 	return (
 		<div className='dashboard'>
 			<div className='topnav'>
